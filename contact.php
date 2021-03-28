32177c040bc88ea9013fe24bc3b4977192bef95c
sethnickolas <56687726+sethnickolas@users.noreply.github.com>
2021-03-28 16:03:08 -0400

    Add files via upload

---
+++ contact.php
@@ -0,0 +1,281 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="utf-8" />
+  <title>Seth Nickolas Photo | Contact</title>
+  <meta name="description" content="Portrait, event and landscape photographer for the Emerald Coast, Florida.">
+  <meta name="author" content="Seth Nickolas">
+
+  <!--Google Font -->
+  <link href='http://fonts.googleapis.com/css?family=Open+Sans+Condensed:300' rel='stylesheet' type='text/css'>
+  <link href='http://fonts.googleapis.com/css?family=Headland+One' rel='stylesheet' type='text/css'>
+
+  <!-- SlidesJS Required (if responsive): Sets the page width to the device width. -->
+  <!--<meta name="viewport" content="width=device-width"> -->
+  <!-- End SlidesJS Required -->
+
+  <!-- CSS for slidesjs.com example -->
+  <link rel="stylesheet" href="../css/example.css">
+
+  <!-- CSS for main page-->
+  <link href="../css/main-page.css" rel="stylesheet" type="text/css">
+
+  <!-- CSS for horizontal menu -->
+  <link href="../css/navbar.css" rel="stylesheet" type="text/css">
+
+  <!-- SlidesJS Required: Link to jQuery Library -->
+  <script src="http://code.jquery.com/jquery-1.9.1.min.js"></script>
+  <!-- End SlidesJS Required -->
+  
+  <!--  Required: Link to jQuery Horiz Menu -->
+  <script src="../jquery/navmenu.js"></script> 
+  
+  
+  
+  
+    <!--style and set the background -->
+
+     
+    <style type="text/css">
+	 
+  body:before {
+	content: "";
+	display: block;
+	position: fixed;
+	left: 0;
+	top: 0;
+	width: 100%;
+	height: 100%;
+	z-index: -2;
+	background: url(../images/grunge_background.jpg) no-repeat center center;
+	-webkit-background-size: cover;
+	-moz-background-size: cover;
+	-o-background-size: cover;
+	background-size: cover;
+  }
+	  
+	 .container {
+	  position: absolute;
+	  border-style:solid;
+      border-width:1px;
+	  left: 50%;
+	  top: 50%;
+	  margin-top: -260px;
+	  margin-left: -470px;
+	  
+	  -moz-box-shadow:    0px 1px 2px 1px #999;
+ 	   -webkit-box-shadow: 0px 1px 2px 1px #999;
+  	   box-shadow:         2px 2px 5px 0px #999;
+	  }
+	  
+	  	#centerParagraph{
+		position:absolute;
+		margin-left: 50px;
+		margin-right: 350px;
+		text-align: justify;
+		text-justify: inter-word;	
+	}
+	  
+label {
+    display:block;
+    margin-top:20px;
+    letter-spacing:2px;
+}
+
+form {
+    position:absolute;
+    width:459px;
+	margin-left: 280px;
+}
+
+/* Style the text boxes */
+input, textarea {
+	width:440px;
+	height:27px;
+	background:#efefef;
+	border:1px solid #dedede;
+	padding:10px;
+	margin-top:3px;
+	font-size:0.9em;
+	color:#3a3a3a;
+}
+
+input, textarea {
+    width:440px;
+    height:27px;
+    background:#efefef;
+    border:1px solid #dedede;
+    padding:10px;
+    margin-top:3px;
+    font-size:0.9em;
+    color:#3a3a3a;
+    border-radius:5px;
+    -moz-border-radius:5px;
+    -webkit-border-radius:5px;
+}
+
+textarea {
+	height:213px;
+}
+
+input:focus, textarea:focus {
+    border:1px solid #97d6eb;
+}
+
+#submit {
+    width:127px;
+    height:38px;
+    background:url(images/submit.jpg);
+    margin-top:20px;
+}
+
+	#submit:hover {
+	    opacity:0.9;
+	}
+	
+
+#myForm{
+	margin-top: 50px;
+}
+
+#messageStatus{
+	position: absolute;	
+	margin-left: 420px;
+	margin-top: 492px;
+}
+
+  </style>
+
+ <script>
+function validateForm()
+{
+var x=document.forms["testForm"]["name"].value;
+var y=document.forms["testForm"]["email"].value;
+var z=document.forms["testForm"]["message"].value;
+if (x==null || x=="" || y==null || y=="" || z==null || z=="" )
+  {
+  alert("Fields must be filled out");
+  return false;
+  }
+}
+</script>
+
+</head>
+<body>
+
+<div id="centerRectangle">
+	<div id="logo">
+	<img src="../images/logo_name.png" alt="Seth Nickolas Photography" />
+	</div>
+   
+    <div id="horizontalMenu">
+    <ul id="jsddm">
+    <li><a href="../index.html">Home</a></li>
+    <li><a href=""; class="active">Galleries</a>
+        <ul>
+            <li><a href="gallery1.html">Couples Portraits</a></li>
+            <li><a href="gallery2.html">Family Portraits</a></li>
+            <li><a href="gallery3.html">Engagement 1</a></li>
+            <li><a href="gallery4.html">Engagement 2</a></li>
+            <li><a href="gallery5.html">Engagement 3</a></li>
+        </ul>
+    </li>
+    <li><a href=""; class="active">Info</a>
+    <ul>
+            <li><a href="pricing.html">Pricing</a></li>
+            <li><a href="bio.html">Bio</a></li>
+        </ul>
+    </li>
+    <li><a href="blog.html">Blog</a></li>
+    <li><a href="contact.php">Contact</a></li>
+</ul>
+</div>
+   
+   
+   
+    
+   <div id="twitterButton">
+   <a href="https://twitter.com/k2burner" class="twitter-follow-button" data-show-count="false" data-show-screen-name="false">Follow @k2burner</a>
+<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');</script>
+    </div>
+    
+    <div id="facebookLogo">
+    <iframe src="http://www.facebook.com/plugins/like.php?href=https%3A%2F%2Fwww.facebook.com%2Fpages%2FSeth-Nickolas-Photography%2F570832049607698&amp;send=false&amp;layout=button_count&amp;width=150&amp;show_faces=false&amp;font&amp;colorscheme=light&amp;action=like&amp;height=21" style="border:none; overflow:hidden; width:150px; height:21px;" allowTransparency="true"></iframe>
+    </div>
+    
+            <!--Horizontal Rule -->
+        <hr id="hr1"><hr width="939">
+        
+        
+<div id="centerParagraph">
+ <h1>Contact</h1>
+</div>
+<!--End Center Paragraph Div-->
+
+<div id="myForm">
+<header class="body">
+    </header>
+
+    <section class="body">
+    </section>
+
+    <footer class="body">
+    </footer>
+    
+    
+    <form name="testForm" method="post" onSubmit="return validateForm()" action="contact.php">
+    
+    <label>Name</label>
+    <input name="name" placeholder="Type Here">
+            
+    <label>Email</label>
+    <input name="email" type="email" placeholder="Type Here">
+            
+    <label>Message</label>
+    <textarea name="message" placeholder="Type Here"></textarea>
+            
+    <input id="submit" name="submit" type="submit" value="Submit">
+</form>
+
+  
+  <div id="messageStatus">
+     <?php
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $message = $_POST['message'];
+    $from = 'From: SNP'; 
+    $to = 'sn215606@gmail.com'; 
+    $subject = 'Photo Inquiry';
+			
+    $body = "From: $name\n E-Mail: $email\n Message:\n $message";
+				
+   if ($_POST['submit']) {
+	if ($name != '' && $email != '') {
+      if (mail ($to, $subject, $body, $from)) { 
+	      echo '<img src="../images/checkmark.png" alt="Check!"/>';
+      } 
+	}
+	  else {
+        echo '<p>Something went wrong!</p>'; 
+    }
+}
+?>
+</div>
+ <!--End message Status Div--> 
+
+</div>
+<!--End my Form Div-->
+  
+  
+</div>
+<!--End Center Rectangle Div-->
+
+<script type='text/javascript'>
+{
+document.getElementById('twitterButton').style.display = 'block';
+}
+</script>
+  <!--  Remove Twitter button if no javascript -->      
+  
+</body>
+</html>
